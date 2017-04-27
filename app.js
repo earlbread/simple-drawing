918eec468fa06d9f6086d8a1b2175669961d6501
Seunghun Lee <waydi1@gmail.com>
2017-04-27 19:13:53 +0900

    Align indentation

--- app.js
+++ app.js
@@ -1,77 +1,77 @@
 (function() {
-    var canvas = document.getElementById('playground');
-    var clearButton = document.getElementById('clear');
-    var context = canvas.getContext('2d');
+  var canvas = document.getElementById('playground');
+  var clearButton = document.getElementById('clear');
+  var context = canvas.getContext('2d');
 
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  context.strokeStyle = "black";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+  var paint;
+
+  var prevX;
+  var prevY;
+
+  function draw(x, y, dragging) {
+    context.beginPath();
+
+    if (dragging) {
+      context.moveTo(prevX, prevY);
+    } else {
+      context.moveTo(x - 1, y);
+    }
+
+    context.lineTo(x, y);
+    context.closePath();
+    context.stroke();
+  }
+
+  var drawStart = function(e) {
+    var x = (e.touches ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
+    var y = (e.touches ? e.touches[0].pageY : e.pageY) - this.offsetTop;
+
+    paint = true;
+
+    prevX = x;
+    prevY = y;
+
+    draw(x, y, false);
+  };
+
+  var drawMove = function(e) {
+    if(paint){
+      var x = (e.touches ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
+      var y = (e.touches ? e.touches[0].pageY : e.pageY) - this.offsetTop;
+
+      draw(x, y, true);
+
+      prevX = x;
+      prevY = y;
+    }
+  };
+
+  var drawEnd = function(e) {
+    paint = false;
+  };
+
+  var drawCancel = function(e) {
+    paint = false;
+  };
+
+  var clearCanvas = function() {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  }
 
-    context.strokeStyle = "black";
-    context.lineJoin = "round";
-    context.lineWidth = 5;
-    var paint;
+  canvas.addEventListener('mousedown', drawStart, false);
+  canvas.addEventListener('mousemove', drawMove, false);
+  canvas.addEventListener('mouseup', drawEnd, false);
+  canvas.addEventListener('mouseleave', drawCancel, false);
 
-    var prevX;
-    var prevY;
+  canvas.addEventListener('touchstart', drawStart, false);
+  canvas.addEventListener('touchmove', drawMove, false);
+  canvas.addEventListener('touchend', drawEnd, false);
+  canvas.addEventListener('touchcancel', drawCancel, false);
 
-    function draw(x, y, dragging) {
-        context.beginPath();
-
-        if (dragging) {
-            context.moveTo(prevX, prevY);
-        } else {
-            context.moveTo(x - 1, y);
-        }
-
-        context.lineTo(x, y);
-        context.closePath();
-        context.stroke();
-    }
-
-    var drawStart = function(e) {
-        var x = (e.touches ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
-        var y = (e.touches ? e.touches[0].pageY : e.pageY) - this.offsetTop;
-
-        paint = true;
-
-        prevX = x;
-        prevY = y;
-
-        draw(x, y, false);
-    };
-
-    var drawMove = function(e) {
-		if(paint){
-            var x = (e.touches ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
-            var y = (e.touches ? e.touches[0].pageY : e.pageY) - this.offsetTop;
-
-            draw(x, y, true);
-
-            prevX = x;
-            prevY = y;
-		}
-    };
-
-    var drawEnd = function(e) {
-        paint = false;
-    };
-
-    var drawCancel = function(e) {
-        paint = false;
-    };
-
-    var clearCanvas = function() {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    }
-
-    canvas.addEventListener('mousedown', drawStart, false);
-    canvas.addEventListener('mousemove', drawMove, false);
-    canvas.addEventListener('mouseup', drawEnd, false);
-    canvas.addEventListener('mouseleave', drawCancel, false);
-
-	canvas.addEventListener('touchstart', drawStart, false);
-	canvas.addEventListener('touchmove', drawMove, false);
-	canvas.addEventListener('touchend', drawEnd, false);
-	canvas.addEventListener('touchcancel', drawCancel, false);
-
-    clear.addEventListener('click', clearCanvas, false);
+  clear.addEventListener('click', clearCanvas, false);
 }());
